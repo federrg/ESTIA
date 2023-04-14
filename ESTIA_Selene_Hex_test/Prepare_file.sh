119ccdf071fd20b041aba511b3d43b222aaca3ac
Federico Rojas <federico.rojas@ess.eu>
2023-04-14 15:15:09 +0200

    Add folder ESTIA_Selene_Hex_test and its content

---
+++ ESTIA_Selene_Hex_test/Prepare_file.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+#Prepare file
+
+#File to modify
+filename="Hex_screw_positions_v1.txt"
+
+grep "^ *Screw(" $filename |
+sed -e "s/    Screw(//g" -e "s/ //g" -e "s/,0.*//g" > GrepTest.txt
+
+
