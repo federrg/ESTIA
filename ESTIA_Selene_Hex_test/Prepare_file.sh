400c002027ef46f03e9aa65b824950b21daa7175
Federico Rojas <federico.rojas@ess.eu>
2023-04-18 15:40:59 +0200

    Add shell script to prepare file to be read

--- ESTIA_Selene_Hex_test/Prepare_file.sh
+++ ESTIA_Selene_Hex_test/Prepare_file.sh
@@ -6,6 +6,6 @@
 filename="Hex_screw_positions_v1.txt"
 
 grep "^ *Screw(" $filename |
-sed -e "s/    Screw(//g" -e "s/ //g" -e "s/,0.*//g" > GrepTest.txt
+sed -e "s/    Screw(//g" -e "s/ //g" -e "s/,0.*//g" > HexKeysPos.csv
 
 
